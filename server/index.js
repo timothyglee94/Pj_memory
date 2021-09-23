0af8117fc935eb418ae32262f352fa4ddfcb2c7e
timothyglee94 <60152531+timothyglee94@users.noreply.github.com>
2021-09-23 14:28:34 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://masteruser:masteruser123@cluster0.hk3dx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL =#;
 const PORT = process.env.PORT || 5000;
 
 mongoose
